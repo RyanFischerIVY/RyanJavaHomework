16654de60bc22e9e5adc515bfbfe0cec32ee5ada
RyanFischerIVY <rfischer29@ivytech.edu>
2025-08-30 10:29:46 -0400

    added some documentation

--- cardNumberChecker.java
+++ cardNumberChecker.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 
 public class cardNumberChecker {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //Creates an object to store the input in
         System.out.print("Enter a credit card number as a long integer: ");
-        long number = input.nextLong();
-
+        long number = input.nextLong(); //Grabs user input
+        //Checks if the number is valid
         if (isValid(number)) {
             System.out.println(number + " is valid.");
         } else {
             System.out.println(number + " is invalid.");
         }
     }
-
+    //Goes through each catagory to find if the number is valid
     public static boolean isValid(long number) {
-        // Luhn algorithm: sum of double even place + sum of odd place % 10 == 0
-        return (sumofDoubleEvenPlace(number) + sumofOddPlace(number)) % 10 == 0
+        return (sumofDoubleEvenPlace(number) + sumofOddPlace(number)) % 10 == 0 //Adds the sums and checks if divisible by 10
                 && (prefixMatched(number, 4) || prefixMatched(number, 5)
-                || prefixMatched(number, 37) || prefixMatched(number, 6))
-                && (getSize(number) >= 13 && getSize(number) <= 16);
+                || prefixMatched(number, 37) || prefixMatched(number, 6)) //Makes sure the prefix is right
+                && (getSize(number) >= 13 && getSize(number) <= 16); //Makes sure the size is right
     }
 
     public static int sumofDoubleEvenPlace(long number) {
@@ -42,7 +41,7 @@ public class cardNumberChecker {
         int sum = 0;
         String num = number + "";
         for (int i = num.length() - 1; i >= 0; i -= 2) {
-            sum += Character.getNumericValue(num.charAt(i));
+            sum += Character.getNumericValue(num.charAt(i)); //Needs to be turned back into a number so it can be added
         }
         return sum;
     }
