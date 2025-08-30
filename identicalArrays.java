5aec337a516dcabff73773895e344f67de134801
RyanFischerIVY <rfischer29@ivytech.edu>
2025-08-30 12:37:23 -0400

    program mostly written

---
+++ identicalArrays.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class identicalArrays {
+    public static void main(String[] args) {
+
+        int rows = 3;
+        int cols = 3;
+        Scanner input = new Scanner(System.in);
+        int[][] m1 = new int[rows][cols];
+        int[][] m2 = new int[rows][cols];
+
+        System.out.println("Enter items for the first list: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Enter number for row " + (i + 1) + " column " + (j + 1) + ": ");
+                m1[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("Enter items for the second list: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print("Enter number for row " + (i + 1) + " column " + (j + 1) + ": ");
+                m1[i][j] = input.nextInt();
+            }
+        }
+
+        if (equals(m1, m2)) {
+            System.out.println("The two arrays are identical.");
+        }
+        else {
+            System.out.println("The two arrays are not identical.");
+        }
+        
+
+
+        input.close();  
+    }
+
+    public static boolean equals (int[][] m1, int[][] m2) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (m1[i][j] != m2[i][j]) {
+                    return false;
+                }
+                return true;
+            }
+        }
+        return true;
+
+    }
+
+}
