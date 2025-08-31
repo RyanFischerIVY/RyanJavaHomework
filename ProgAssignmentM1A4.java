94b35f9add25bd4d61fa3076d5f9da0dfa08c5c8
RyanFischerIVY <rfischer29@ivytech.edu>
2025-08-31 13:27:07 -0400

    wrote regularPolygon class

---
+++ ProgAssignmentM1A4.java
@@ -0,0 +1,73 @@
+public class ProgAssignmentM1A4 {
+    public static void Main() {
+        
+        
+    }
+
+
+
+
+    //Class for the regular polygon with all its methods
+    class regularPolygon {
+        private int n = 3;
+        private double side = 1.0;
+        private double x = 0.0;
+        private double y = 0.0;
+        //3 contructors to serve differnt roles
+        public regularPolygon() {
+
+        }
+
+        public regularPolygon(int n, double side) {
+            this.n = n;
+            this.side = side;
+        }
+
+        public regularPolygon(int n, double side, double x, double y) {
+            this.n = n;
+            this.side = side;
+            this.x = x;
+            this.y = y;
+        }
+        //Getters and setters for all important variables
+        public int getN() {
+            return n;
+        }
+
+        public void setN(int n) {
+            this.n = n;
+        }
+
+        public double getSide() {
+            return side;
+        }
+
+        public void setSide(double side) {
+            this.side = side;
+        }
+
+        public double getX() {
+            return x;
+        }
+
+        public void setX(double x) {
+            this.x = x;
+        }
+
+        public double getY() {
+            return y;
+        }
+
+        public void setY(double y) {
+            this.y = y;
+        }
+        //Formula to find the perimeter
+        public double getPerimeter() {
+            return n * side;
+        }
+        //Formula to find the area
+        public double getArea() {
+            return (n * Math.pow(side, 2) / (4 * Math.tan(Math.PI / n)));
+        }
+    }
+}
