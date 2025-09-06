302fc3fc15487c90a19e3bc348ee6eca53dd66ee
RyanFischerIVY <rfischer29@ivytech.edu>
2025-09-06 11:44:53 -0400

    wrote the base class, need to fill in methods

---
+++ ProgAssignmentM2A1.java
@@ -0,0 +1,53 @@
+// ProgAssignmentM2A1.java
+// Ryan Fischer
+// 9/6/25
+// Professor Parrot
+
+
+
+
+
+public class ProgAssignmentM2A1 {
+    public static void main(String[] args) {
+
+    }
+
+    class MyDate {
+        private int year;
+        private int month;
+        private int day;
+
+        public MyDate() {
+
+        }
+
+        public MyDate(long elapsedTime) {
+            
+        }
+
+        public MyDate(int year, int month, int day) {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+
+        public int getYear() {
+            return year;
+        }
+
+        public int getMonth() {
+            return month;
+        }
+
+        public int getDay() {
+            return day;
+        }
+
+        public void setDate(long elapsedTime) {
+
+        }
+
+    
+
+    }
+}
