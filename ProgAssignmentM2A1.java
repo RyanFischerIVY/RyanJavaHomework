4188c5b232ff25685bfa1a84f2d9ae4c7284de4d
RyanFischerIVY <rfischer29@ivytech.edu>
2025-09-06 12:51:14 -0400

    filled in the methods

--- ProgAssignmentM2A1.java
+++ ProgAssignmentM2A1.java
@@ -3,26 +3,32 @@
 // 9/6/25
 // Professor Parrot
 
-
-
-
+import java.util.GregorianCalendar;
 
 public class ProgAssignmentM2A1 {
     public static void main(String[] args) {
+        MyDate date1 = new MyDate();
+        MyDate date2 = new MyDate(561555550000L);
+
+        System.out.println("Date 1: " + date1.getYear() + "-" + date1.getMonth() + "-" + date1.getDay());
+        System.out.println("Date 2: " + date2.getYear() + "-" + date2.getMonth() + "-" + date2.getDay());
 
     }
 
-    class MyDate {
-        private int year;
-        private int month;
-        private int day;
+    static class MyDate {
+        private int year = 1970;
+        private int month = 1;
+        private int day = 1;
 
         public MyDate() {
-
+            GregorianCalendar calendar = new GregorianCalendar();
+            this.year = calendar.get(GregorianCalendar.YEAR);
+            this.month = calendar.get(GregorianCalendar.MONTH) + 1;
+            this.day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
         }
 
         public MyDate(long elapsedTime) {
-            
+            setDate(elapsedTime);
         }
 
         public MyDate(int year, int month, int day) {
@@ -44,6 +50,11 @@ public class ProgAssignmentM2A1 {
         }
 
         public void setDate(long elapsedTime) {
+            java.util.GregorianCalendar calendar = new java.util.GregorianCalendar();
+            calendar.setTimeInMillis(elapsedTime);
+            this.year = calendar.get(GregorianCalendar.YEAR);
+            this.month = calendar.get(GregorianCalendar.MONTH);
+            this.day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
 
         }
 
