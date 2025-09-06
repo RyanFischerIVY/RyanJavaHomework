7e8fbfbab9f8d67b338a3bf6769f7459c2d0c9ed
RyanFischerIVY <rfischer29@ivytech.edu>
2025-09-06 12:59:35 -0400

    documented and finished

--- ProgAssignmentM2A1.java
+++ ProgAssignmentM2A1.java
@@ -7,36 +7,36 @@ import java.util.GregorianCalendar;
 
 public class ProgAssignmentM2A1 {
     public static void main(String[] args) {
+        //Creates the objects
         MyDate date1 = new MyDate();
-        MyDate date2 = new MyDate(561555550000L);
-
+        MyDate date2 = new MyDate(34355555133101L);
+        //Prints out the date specified
         System.out.println("Date 1: " + date1.getYear() + "-" + date1.getMonth() + "-" + date1.getDay());
         System.out.println("Date 2: " + date2.getYear() + "-" + date2.getMonth() + "-" + date2.getDay());
-
     }
 
     static class MyDate {
-        private int year = 1970;
-        private int month = 1;
-        private int day = 1;
-
+        private int year;
+        private int month;
+        private int day;
+        //Gets the current date
         public MyDate() {
             GregorianCalendar calendar = new GregorianCalendar();
             this.year = calendar.get(GregorianCalendar.YEAR);
             this.month = calendar.get(GregorianCalendar.MONTH) + 1;
             this.day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
         }
-
+        //Finds the date since 1970 using miliseconds
         public MyDate(long elapsedTime) {
             setDate(elapsedTime);
         }
-
+        //Lets the user enter their own date
         public MyDate(int year, int month, int day) {
             this.year = year;
             this.month = month;
             this.day = day;
         }
-
+        //Getters
         public int getYear() {
             return year;
         }
@@ -48,12 +48,12 @@ public class ProgAssignmentM2A1 {
         public int getDay() {
             return day;
         }
-
+        //Sets teh date using the elapsed time
         public void setDate(long elapsedTime) {
             java.util.GregorianCalendar calendar = new java.util.GregorianCalendar();
             calendar.setTimeInMillis(elapsedTime);
             this.year = calendar.get(GregorianCalendar.YEAR);
-            this.month = calendar.get(GregorianCalendar.MONTH);
+            this.month = calendar.get(GregorianCalendar.MONTH) + 1;
             this.day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
 
         }
