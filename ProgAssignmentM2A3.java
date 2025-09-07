6179cf791962a5c7d6543228e11f6f3eb0e98c56
RyanFischerIVY <rfischer29@ivytech.edu>
2025-09-07 12:50:07 -0400

    did the requirments for 12.7

---
+++ ProgAssignmentM2A3.java
@@ -0,0 +1,29 @@
+
+
+import java.util.Scanner;
+
+public class ProgAssignmentM2A3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Please enter a string binary value: ");
+        String binaryString = input.nextLine();
+        
+        try {
+        int decimal = bin2dec(binaryString);
+        System.out.println("The converted decimal number is: " + decimal);
+        } 
+        catch (NumberFormatException e) {
+        System.out.println("Invalid binary string.");
+        }
+
+
+        input.close();
+    }
+
+    public static int bin2dec(String binaryString) throws NumberFormatException {
+
+        int decimal = Integer.parseInt(binaryString, 2);
+
+        return decimal;
+    }
+}
