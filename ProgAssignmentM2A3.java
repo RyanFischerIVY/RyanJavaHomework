e51b37b9b06902b18e70646cbf4a8aeb4aa26fe5
RyanFischerIVY <rfischer29@ivytech.edu>
2025-09-07 13:53:36 -0400

    added documentation

--- ProgAssignmentM2A3.java
+++ ProgAssignmentM2A3.java
@@ -1,13 +1,18 @@
+//Program Assignment M2A3
+//Ryan Fischer
+//9-7-2025
+//Professor Parrot
 
 
 import java.util.Scanner;
 
 public class ProgAssignmentM2A3 {
     public static void main(String[] args) {
+        //Input section
         Scanner input = new Scanner(System.in);
         System.out.print("Please enter a string binary value: ");
         String binaryString = input.nextLine();
-        
+        //Converts the string to a decimal value
         try {
         int decimal = bin2dec(binaryString);
         System.out.println("The converted decimal number is: " + decimal);
@@ -21,18 +26,17 @@ public class ProgAssignmentM2A3 {
     }
 
         public static int bin2dec(String binaryString) throws BinaryFormatException {
-            // Manual validation
+            //Method used for the conversion
             for (char c : binaryString.toCharArray()) {
                 if (c != '0' && c != '1') {
                     throw new BinaryFormatException("Not a binary string: " + binaryString);
                 }
             }
 
-            // Safe conversion
             return Integer.parseInt(binaryString, 2);
         }
-
-    public class BinaryFormatException extends Exception {
+        //Custom exception class
+    public static class BinaryFormatException extends Exception {
         public BinaryFormatException(String message) {
             super(message);
 
