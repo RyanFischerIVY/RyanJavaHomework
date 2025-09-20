e7b6a04e323fbb67daa4d5dd4d19a4de98119f6b
RyanFischerIVY <rfischer29@ivytech.edu>
2025-09-20 12:30:30 -0400

    wrote a little more

--- ProgAssignmentM4A3.java
+++ ProgAssignmentM4A3.java
@@ -65,7 +65,69 @@ public class ProgAssignmentM4A3 {
         @Override
         public void add(int index, E e) {
             if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+            if (index == 0) {
+                addFirst(e);
+                return;
+            }
+            if (index == size) {
+                addLast(e);
+                return;
+            }
+            Node<E> current = getNode(index);
+            Node<E> newNode = new Node<>(e);
+
+            newNode.previous = current.previous;
+            newNode.next = current;
+            current.previous.next = newNode;
+            current.previous = newNode;
+            size++;
+        }
+
+        public E removeFirst() {
+            if (size == 0) return null;
+            Node<E> temp = head;
+            head = head.previous;
+            if (head != null) {
+                head.next = null;
+            }
+            else {
+                tail = null;
+            }
+            size--;
+            return temp.element;
+        }
+
+        public E removeLast() {
+            if (size == 0) return null;
+            Node<E> temp = tail;
+            tail = tail.previous;
+            if (tail != null) {
+                tail.next = null;
+            }
+            else {
+                head = null;
+            }
+            size--;
+            return temp.element;
+        }
+
+        @Override
+        public void remove(int index) {
+            if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+            if (index == 0) {
+                removeFirst();
+                return;
+            }
+            if (index == size) {
+                removeLast();
+                return;
+            }
             
+            Node<E> current = getNode(index);
+            current.previous.next = current.next;
+            current.next.previous = current.previous;
+            size--;
+            return current.element;
         }
     }
 }
